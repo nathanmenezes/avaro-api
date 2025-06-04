b6b0fa71dad726038bc5ab0e3b5c7b6d0413e27c
NathanMenezez <nathan.silva@cruzvita.com>
2025-06-03 21:53:48 -0300

    Initial Commit

---
+++ src/main/java/br/com/mnz/avaroapi/AvaroapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.mnz.avaroapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaroapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvaroapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/mnz/avaroapi/AvaroapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mnz.avaroapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaroapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
